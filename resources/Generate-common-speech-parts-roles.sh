6d91bebee38a66b7a567b821a6fd7bd8427ca319
antononcube <antononcube@posteo.net>
2023-01-03 08:18:36 -0500

    fix:Using the updated script name from Grammar::TokenProcessing.

--- resources/Generate-common-speech-parts-roles.sh
+++ resources/Generate-common-speech-parts-roles.sh
@@ -7,7 +7,7 @@
 echo 'Process English template'
 
 cd ./lib/DSL/Shared/Roles/English
-add-fuzzy-matching -o=./CommonSpeechParts.rakumod --add-protos --sym-name=English --add-exclusions ./CommonSpeechParts-template
+add-token-fuzzy-matching -o=./CommonSpeechParts.rakumod --add-protos --sym-name=English --add-exclusions ./CommonSpeechParts-template
 
 # Go back to the top directory
 cd ../../../../../
@@ -16,7 +16,7 @@ cd ../../../../../
 echo 'Process Bulgarian template'
 
 cd ./lib/DSL/Shared/Roles/Bulgarian
-add-fuzzy-matching -o=./CommonSpeechParts.rakumod --/add-protos --sym-name=Bulgarian --add-exclusions --func-name=is-bg-fuzzy-match ./CommonSpeechParts-template
+add-token-fuzzy-matching -o=./CommonSpeechParts.rakumod --/add-protos --sym-name=Bulgarian --add-exclusions --func-name=is-bg-fuzzy-match ./CommonSpeechParts-template
 
 # Go back to the top directory
 cd ../../../../../
